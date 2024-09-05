a0c33b1ee31ce6d6da0bb989625962a58d73c4c0
Weverson Godinho <weverson.godinho@icloud.com>
2024-09-05 14:35:36 +0000

    Added the cursor pointer and borders on inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ const peopleInput = document.getElementById('peopleInput')
 const btnTips = [...document.querySelectorAll('.tip')]
 const btnCustom = document.getElementById('custom')
 const inputWrapper = document.querySelector('.input-wrapper')
+const inputValue = document.querySelector('.input-value')
 const borderNumberPeople = document.querySelector('.borderNumberPeople')
 const peopleLabelBox = document.querySelector('.people-label-box')
-const billLabelBox = document.querySelector('.bill-label-box')
 
 let tipsOn
 let tips
@@ -13,24 +13,49 @@ let custom
 let numberPerson
 let bill
 
+
+const emptyField = () => {
+      //Change border of input 
+      inputWrapper.style.border = '2px solid #E17457'
+      borderNumberPeople.style.border = '2px solid #E17457'
+
+      //Create a new label for both inputs
+      let label = document.createElement('label')
+      label.innerHTML = "Can't be zero"
+      label.style.color = '#E17457'
+
+      peopleLabelBox.appendChild(label)
+      peopleLabelBox.style.display = 'flex'
+      peopleLabelBox.style.justifyContent = 'space-between';
+      peopleLabelBox.style.width = '100'
+
+      bill = ''
+      tips = ''
+      custom = ''
+}
+
+// Catch the Bill value
 billInput.addEventListener('change',function() {
     bill = billInput.value
     incrementTips() 
 })
 
-
+// Catch the Select Tip value
 btnTips.forEach((btn) =>{
     btn.addEventListener('click', function () {
+    btn.style.backgroundColor = '#26C2AE'
     tips = btn.textContent
     incrementTips() 
    } )
 })
  
+// Catch the Custom Tip value
 btnCustom.addEventListener('change',function() {
     custom = btnCustom.value
     incrementTips() 
 })
 
+// Catch the number of people value
 peopleInput.addEventListener('change',function() {
     numberPerson = peopleInput.value
     incrementTips() 
@@ -39,25 +64,19 @@ peopleInput.addEventListener('change',function() {
 const incrementTips = () =>{
     try {
         if (bill) {
-            if (tips) {
-                console.log(bill);
-                console.log(tips);
+            if (tips && numberPerson) {
+                let tip = Number(tips.replace('%', ''))
+                let porcentageValue = (tip / 100) * Number(bill)
+                let tipAmount = porcentageValue / Number(numberPerson)
+                console.log(tipAmount);
+                
             } else if (custom) { 
                 console.log(bill);
                 console.log(custom);
             }
         } else {
-            inputWrapper.style.border = '2px solid #E17457'
-            borderNumberPeople.style.border = '2px solid #E17457'
-            let label = document.createElement('label')
-            label.innerHTML = "Can't be zero"
-            label.style.color = '#E17457'
-            peopleLabelBox.appendChild(label)
-            peopleLabelBox.style.display = 'flex'
-            peopleLabelBox.style.justifyContent = 'space-between';
-            peopleLabelBox.style.width = '100'
-            console.log('The bill is empty');
-        }
+          emptyField()
+         }  
     } catch (error) { // Include error object to log the error
         console.log('An error occurred:', error.message);
     }
